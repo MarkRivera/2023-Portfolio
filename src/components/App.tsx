dcf61490ea947723a1f03ec5c5d2febe2b2d55c6
Mark Rivera <Mrivera1991@gmail.com>
2023-02-25 16:23:30 -0800

    initialized new portfolio

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import Avatar from 'components/Avatar'
+
+function App() {
+  return (
+    <></>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+			colors: {
+				primary: "#11001c",
+        secondary: "#e38125",
+				// Need color for my name and links
+				// Need color for social media
+				// Hover states
+			}
+		},
+  },
+  plugins: []
+}
