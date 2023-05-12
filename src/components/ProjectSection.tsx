af1dfa0f6d76429b1f8eb8862e3c13f584aa9e11
Mark Rivera <Mrivera1991@gmail.com>
2023-05-12 16:28:50 -0400

    updates to portfolio

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,86 +1,142 @@
-import Section from "./Section"
+import Section from "./Section";
 
 type Project = {
-  name: string
-  description: string
-  techStack: string[]
-  github: string
-  live: string | null
-}
+  name: string;
+  description: string;
+  techStack: string[];
+  github: string;
+  live: string | null;
+};
 
-type ProjectData = Project[]
+type ProjectData = Project[];
 
 const ProjectCard = ({ project }: { project: Project }) => {
-  return <div className="flex flex-col w-72 sm:w-1/2 bg-primary rounded-2xl p-8">
-    <h2 className="text-2xl font-semibold mb-4 text-gradient bg-clip-text text-transparent">{project.name}</h2>
-    <p className="text-sm mb-4">
-      {project.description}
-    </p>
-    <div className="flex">
-      <div className="flex flex-col">
-        <h3 className="text-sm font-semibold mb-2">Tech Stack</h3>
-        <ul className="flex flex-wrap">
-          {project.techStack.map((tech, index) => {
-            return <li key={index} className="mr-2">{tech}</li>
-          })}
-        </ul>
-        <div className="font-bold text-gradient bg-clip-text text-transparent mt-4">
-          <a
-            className="mr-4"
-            href={project.github} target="blank">GitHub</a>
-          {project.live && <a
-            href={project.live}
-            target="blank"
-          >Live</a>}
+  return (
+    <div className="flex w-full flex-col rounded-2xl bg-primary p-8 sm:w-1/2">
+      <h2 className="text-gradient mb-4 bg-clip-text text-2xl font-semibold text-transparent">
+        {project.name}
+      </h2>
+      <p className="mb-4 text-sm">{project.description}</p>
+      <div className="flex">
+        <div className="flex flex-col">
+          <h3 className="mb-2 text-sm font-semibold">Tech Stack</h3>
+          <ul className="flex flex-wrap">
+            {project.techStack.map((tech, index) => {
+              return (
+                <li key={index} className="mr-2">
+                  {tech}
+                </li>
+              );
+            })}
+          </ul>
+          <div className="text-gradient mt-4 bg-clip-text font-bold text-transparent">
+            <a className="mr-4" href={project.github} target="blank">
+              GitHub
+            </a>
+            {project.live && (
+              <a href={project.live} target="blank">
+                Live
+              </a>
+            )}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-}
+  );
+};
 
 const Projects = () => {
   const projects: ProjectData = [
     {
       name: "Glimpse",
-      description: "Glimpse is a state of the art AI powered web application that allows users to empower investigations and data parsing. Users can create an account, create projects, Upload and view videos from other users. Users can also view a table of all videos and filter by multiple categories.",
-      techStack: ["React", "NextJS", "tRPC", "PostgreSQL", "TypeScript", "TailwindCSS", "Prisma"],
-      github: "https://github.com/Human-Rights-First-Innovation-Lab/glimpse-web",
+      description:
+        "Glimpse is a state of the art AI powered web application that allows users to empower investigations and data parsing. Users can create an account, create projects, Upload and view videos from other users. Users can also view a table of all videos and filter by multiple categories.",
+      techStack: [
+        "React",
+        "NextJS",
+        "tRPC",
+        "PostgreSQL",
+        "TypeScript",
+        "TailwindCSS",
+        "Prisma"
+      ],
+      github:
+        "https://github.com/Human-Rights-First-Innovation-Lab/glimpse-web",
       live: "https://glimpse-neon.vercel.app/"
     },
     {
       name: "Maximum Earnings",
-      description: "A small responsive calculator built to accept any number of students and their respective potential profits. The calculator will then calculate the maximum earnings for the given students within a number of give hours.",
+      description:
+        "A small responsive calculator built to accept any number of students and their respective potential profits. The calculator will then calculate the maximum earnings for the given students within a number of give hours.",
       techStack: ["React", "Vite", "TypeScript", "TailwindCSS"],
       github: "https://github.com/MarkRivera/maximum-earnings",
       live: "https://maximum-earnings.vercel.app/"
     },
     {
+      name: "MikeMerch",
+      description:
+        "A simple portfolio built with React and NextJS for an musician / actor. Currently in development. Enter via the Merch side of the site.",
+      techStack: ["React", "NextJS", "TypeScript", "TailwindCSS"],
+      github: "https://github.com/MarkRivera/next-merch",
+      live: "https://quirky-kilby-e5e908.netlify.app/"
+    },
+    {
       name: "Tic Tac Toe",
-      description: "A simple tic tac toe game built for the CLI with TypeScript. The game allows users to play against another player. Want to play?",
+      description:
+        "A simple tic tac toe game built for the CLI with TypeScript. The game allows users to play against another player. Want to play?",
       techStack: ["TypeScript", "NodeJS"],
       github: "https://github.com/MarkRivera/tic-tac-toe",
       live: null
     },
     {
-      name: "Pomodoro (Work in Progress)",
-      description: "Unhappy with the current available pomodoro timers, I decided to build my own. This pomodoro timer allows users to set a custom work and break time. The timer will also play a custom sound when the timer is up.",
+      name: "React Pomodoro (Work in Progress)",
+      description:
+        "Unhappy with the current available pomodoro timers, I decided to build my own. This pomodoro timer allows users to set a custom work and break time. The timer will also play a custom sound when the timer is up.",
       techStack: ["React", "Rust", "Tauri", "TypeScript"],
       github: "https://github.com/MarkRivera/pomodoro",
       live: null
+    },
+    {
+      name: "Solid Pomodoro (Work in Progress)",
+      description:
+        "Same as the React Pomo but built with SolidJS. This pomodoro timer allows users to set a custom work and break time. The timer will also play a custom sound when the timer is up.",
+      techStack: ["SolidJS", "Rust", "Tauri", "TypeScript"],
+      github: "https://github.com/MarkRivera/solid-pomo",
+      live: null
+    },
+    {
+      name: "Human Rights First - Blue Witness",
+      description:
+        "Blue Witness is a web application that allows users to research police misconduct in the United States. Users can view a map of the United States and view police misconduct in their area. Users can also view a table of all police misconduct and filter by multiple categories.",
+      techStack: [
+        "React",
+        "JavaScript",
+        "Python",
+        "Redux",
+        "ChartJS",
+        "Ant Design"
+      ],
+      github:
+        "https://github.com/BloomTech-Labs/human-rights-first-police-fe-a",
+      live: null
     }
-  ]
+  ];
 
-  return <section className="flex flex-col sm:flex-row flex-wrap">
-    {projects.map((project, index) => {
-      return <ProjectCard key={index} project={project} />
-    })}
-  </section>
-}
+  return (
+    <section className="flex flex-col flex-wrap sm:flex-row sm:justify-center">
+      {projects.map((project, index) => {
+        return <ProjectCard key={index} project={project} />;
+      })}
+    </section>
+  );
+};
 
 const ProjectSection = () => {
   return (
-    <Section title="Projects" number="03" children={<Projects />} id="projects" />
-  )
-}
+    <Section title="Projects" number="03" id="projects">
+      <Projects />
+    </Section>
+  );
+};
 
-export default ProjectSection
+export default ProjectSection;
--- src/components/ResumeSection.tsx
+++ src/components/ResumeSection.tsx
@@ -4,14 +4,14 @@ import Section from "./Section";
 import { Arrow } from "./Arrow";
 
 type WorkHistory = {
-  company: string
-  position: string
-  startDate: string
-  endDate: string
-  responsibilities: string[]
+  company: string;
+  position: string;
+  startDate: string;
+  endDate: string;
+  responsibilities: string[];
 };
 
-type ResumeData = WorkHistory[]
+type ResumeData = WorkHistory[];
 
 const Resume = () => {
   const data: ResumeData = [
@@ -20,76 +20,105 @@ const Resume = () => {
       position: "Lead Software Engineer",
       startDate: "Jan 2022",
       endDate: "February 2023",
-      responsibilities: ["Develop, document and maintain the startup's AI powered web interface using React, TypeScript, Postgres and NextJS", "Collaborate with designers, engineers and product managers to build new features", "Write modern, performant, and maintainable code"]
+      responsibilities: [
+        "Developed machine learning-powered full stack web application to parse human rights video data to reduce manual investigation by more than 50 %+.",
+        "Partnered with machine learning engineer and designer to solicit feedback showing an improvement of positive user experience by 80 %.",
+        "Utilized PaaS to upload and pipeline videos into machine learning model to increase accuracy by 5%."
+      ]
     },
     {
       company: "QuotaPath",
       position: "Software Engineer",
       startDate: "Aug 2021",
       endDate: "Apr 2022",
-      responsibilities: ["Improved render performance by 96% through dependency upgrades and code refactoring", "Contributed to the development and maintenance of a Django/React application", "Worked closely with UI/UX and QA teams to ensure product stability and user retention"]
+      responsibilities: [
+        "Optimized React application performance through caching, memoization, and dependency management. Reducing unnecessary rerenders by 96 %.",
+        "Updated and maintained a Django / Python backend with a PostgreSQL database.",
+        "Authored Unit and E2E tests with tools such as Cypress and CircleCI to maintain at least 75% test coverage and 99.999 % uptime."
+      ]
     },
     {
       company: "SvnStar",
       position: "Software Engineer",
       startDate: "Feb 2021",
       endDate: "Nov 2021",
-      responsibilities: ["Developed a React/Redux web application for a startup that provides a platform for users to safely interact with US law enforcement", "Authored test suites to ensure app stability and redundancy", "Write modern, performant, and maintainable code"]
+      responsibilities: [
+        "Collaborated with 3 engineers to create a SaaS to enable safe interactions with public law enforcement. Culminating in an acceptance by the AWS accelerator program",
+        "Designed and engineered technical spec sheets and beta application for investors, securing $800,000+ in investment capital.",
+        "Authored a comprehensive suite of unit tests to enable a migration from Next.js to AWS Amplify without the loss of functionality.",
+        "Coordinated with the Project Manager to produce technical specs and working prototype that was presented to the City of Chicago and Port Authority."
+      ]
     },
     {
       company: "The Orr Group (Roll20)",
       position: "Junior Software Engineer",
       startDate: "Aug 2020",
       endDate: "Jan 2021",
-      responsibilities: ["Authored and maintained over 20+ virtual tabletop gaming sheets which are used by over 8 million players", "Resolved over 100+ bugs and feature requests in a 6 month period", "Collaborated with UI/UX to design wireframes and mockups for new features"]
+      responsibilities: [
+        "Developed and updated over 20+ virtual tabletop gaming sheets, which are used by over 8,000,000 users.",
+        "Resolved 8 to 10 user generated tickets daily, which resulted in consistently updated sheets and improved user satisfaction.",
+        "Designed wireframes for multiple sheets, improving team coordination and development time on long or complex projects."
+      ]
     }
   ];
 
   const [activeCompany, setActiveCompany] = useState<WorkHistory>(data[0]);
 
   const handleCompanyClick = (company: WorkHistory) => {
-    setActiveCompany(company)
-  }
+    setActiveCompany(company);
+  };
 
-  return <section className="flex flex-col mb-16">
-    <div className="flex w-full overflow-x-auto">
-      <ul className="flex w-full ml-4 scrollbar-thin scrollbar-track-primary scrollbar-thumb-secondary scrollbar-thumb-rounded">
-        {data.map((item, index) => {
-          return <li key={index} className={cx("w-max mt-4 px-4 h-12 whitespace-nowrap cursor-pointer",
-            {
-              "sm:border-b-secondary sm:border-b-2 font-semibold text-secondary sm:text-white": activeCompany.company === item.company,
-            })
-          }
-            onClick={() => handleCompanyClick(item)}>
-            {item.company}
-          </li>
-        })
-        }
-      </ul>
-    </div>
-    <div className="px-12 mt-8">
-      <h2 className="mb-2">
-        {`${activeCompany.position} @ `}
-        <span className="text-secondary font-semibold">
-          {`${activeCompany.company}`}
-        </span>
-      </h2>
-      <h3 className="mb-4">{`${activeCompany.startDate} - ${activeCompany.endDate}`}</h3>
-      <ul>
-        {activeCompany.responsibilities.map((item, index) => {
-          return <li key={index} className="mb-4">
-            <Arrow /> {item}
-          </li>
-        })}
-      </ul>
-    </div>
-  </section>
-}
+  return (
+    <section className="mb-16 flex flex-col">
+      <div className="flex w-full overflow-x-auto">
+        <ul className="ml-4 flex w-full scrollbar-thin scrollbar-track-primary scrollbar-thumb-secondary scrollbar-thumb-rounded">
+          {data.map((item, index) => {
+            return (
+              <li
+                key={index}
+                className={cx(
+                  "w-max mt-4 px-4 h-12 whitespace-nowrap cursor-pointer",
+                  {
+                    "sm:border-b-secondary sm:border-b-2 font-semibold text-secondary sm:text-white":
+                      activeCompany.company === item.company
+                  }
+                )}
+                onClick={() => handleCompanyClick(item)}
+              >
+                {item.company}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="mt-8 px-12">
+        <h2 className="mb-2">
+          {`${activeCompany.position} @ `}
+          <span className="font-semibold text-secondary">
+            {`${activeCompany.company}`}
+          </span>
+        </h2>
+        <h3 className="mb-4">{`${activeCompany.startDate} - ${activeCompany.endDate}`}</h3>
+        <ul>
+          {activeCompany.responsibilities.map((item, index) => {
+            return (
+              <li key={index} className="mb-4">
+                <Arrow /> {item}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 const ResumeSection = () => {
   return (
-    <Section title="Resume" number="02" children={<Resume />} id="resume" />
-  )
-}
+    <Section title="Resume" number="02" id="resume">
+      <Resume />
+    </Section>
+  );
+};
 
-export default ResumeSection
+export default ResumeSection;
